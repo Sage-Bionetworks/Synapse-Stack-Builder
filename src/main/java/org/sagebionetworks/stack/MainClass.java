afe7409e60ca07a6e5220dbd48520e39548b86f4
john-hill <john.hill@sagebase.org>
2012-07-17 13:00:00 -0700

    initial project setup

---
+++ src/main/java/org/sagebionetworks/stack/MainClass.java
@@ -0,0 +1,13 @@
+package org.sagebionetworks.stack;
+
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/org/sagebionetworks/MainClassTest.java
@@ -0,0 +1,5 @@
+package org.sagebionetworks;
+
+public class MainClassTest {
+
+}
