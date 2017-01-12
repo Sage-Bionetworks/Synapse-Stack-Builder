790e55bfa8958ad1b39c9d5c9e0eaa4fd6cb5042
xavier <xschildwachter@hotmail.com>
2017-01-12 06:53:59 -0800

    Fix min/max instances for repo/non-repo.

--- src/main/java/org/sagebionetworks/stack/ElasticBeanstalkSetup.java
+++ src/main/java/org/sagebionetworks/stack/ElasticBeanstalkSetup.java
@@ -81,7 +81,6 @@ public class ElasticBeanstalkSetup implements ResourceProcessor {
 	/**
 	 * The IoC constructor.
 	 * 
-	 * @param beanstalkClient
 	 * @param config
 	 * @param resources
 	 */
@@ -501,13 +500,13 @@ public class ElasticBeanstalkSetup implements ResourceProcessor {
 					if("MinSize".equals(name)){
 						if(Long.parseLong(value) < 8){
 							logger.debug("Overriding aws.autoscaling.asg.MinSize for production to be at least 4");
-							value = "8";
+							value = (env.equals(StackEnvironmentType.REPO)) ? "8" : "4";
 						}
 					}
 					if("MaxSize".equals(name)){
 						if(Long.parseLong(value) < 12){
 							logger.debug("Overriding aws.autoscaling.asg.MaxSize for production to be at least 8");
-							value = "12";
+							value = (env.equals(StackEnvironmentType.REPO)) ? "12" : "8";
 						}
 					}
 					// We want our two instances to be in any two zones. See PLFM-1560
