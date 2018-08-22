62289ed1e6c9382d84b44aa7fe7615a3b7a715fa
john-hill <john.hill@sagebase.org>
2018-08-21 17:06:26 -0700

    id gen database name includes color

--- src/main/java/org/sagebionetworks/template/repo/IdGeneratorBuilderImpl.java
+++ src/main/java/org/sagebionetworks/template/repo/IdGeneratorBuilderImpl.java
@@ -1,10 +1,13 @@
 package org.sagebionetworks.template.repo;
 
+import static org.sagebionetworks.template.Constants.DATABASE_IDENTIFIER;
 import static org.sagebionetworks.template.Constants.JSON_INDENT;
 import static org.sagebionetworks.template.Constants.PROPERTY_KEY_STACK;
+import static org.sagebionetworks.template.Constants.PROPERTY_KEY_VPC_SUBNET_COLOR;
 import static org.sagebionetworks.template.Constants.STACK;
 import static org.sagebionetworks.template.Constants.TEMPLATE_ID_GENERATOR;
 import static org.sagebionetworks.template.Constants.VPC_EXPORT_PREFIX;
+import static org.sagebionetworks.template.Constants.VPC_SUBNET_COLOR;
 
 import java.io.StringWriter;
 
@@ -45,9 +48,13 @@ public class IdGeneratorBuilderImpl implements IdGeneratorBuilder {
 	@Override
 	public void buildAndDeploy() {
 		VelocityContext context = new VelocityContext();
+		String color = config.getProperty(PROPERTY_KEY_VPC_SUBNET_COLOR);
 		String stack = config.getProperty(PROPERTY_KEY_STACK);
+		String databaseIdentifier = stack+"-id-generator-db-"+color;
 		context.put(STACK, stack);
 		context.put(VPC_EXPORT_PREFIX, Constants.createVpcExportPrefix(stack));
+		context.put(VPC_SUBNET_COLOR, color);
+		context.put(DATABASE_IDENTIFIER, databaseIdentifier);
 
 		Parameter parameter = new Parameter();
 		parameter.withParameterKey(Constants.PARAMETER_MYSQL_PASSWORD);
@@ -63,7 +70,8 @@ public class IdGeneratorBuilderImpl implements IdGeneratorBuilder {
 		JSONObject templateJson = new JSONObject(resultJSON);
 		// Format the JSON
 		resultJSON = templateJson.toString(JSON_INDENT);
-		String stackName = stack + "-id-generator";
+		System.out.println(resultJSON);
+		String stackName = stack + "-id-generator-"+color;
 		this.logger.info("Template for stack: " + stackName);
 		this.logger.info(resultJSON);
 		// create or update the template
