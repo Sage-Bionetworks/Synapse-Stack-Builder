2043bb407a2d34be47d238334c9887405b29b7d1
john-hill <john.hill@sagebase.org>
2018-11-06 17:51:48 -0800

    added logging while building wars

--- src/main/java/org/sagebionetworks/war/WarAppenderImpl.java
+++ src/main/java/org/sagebionetworks/war/WarAppenderImpl.java
@@ -6,13 +6,18 @@ import java.nio.file.Files;
 import java.util.function.Consumer;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.Logger;
+import org.sagebionetworks.template.LoggerFactory;
 
 import com.google.inject.Inject;
 
 public class WarAppenderImpl implements WarAppender {
 	
+	Logger logger;
+	
 	@Inject
-	public WarAppenderImpl() {
+	public WarAppenderImpl(LoggerFactory loggerFactory) {
+		logger = loggerFactory.getLogger(WarAppenderImpl.class);
 	}
 
 	@Override
@@ -20,12 +25,14 @@ public class WarAppenderImpl implements WarAppender {
 		File tempDir = null;
 		try {
 			tempDir = Files.createTempDirectory("warCopy").toFile();
+			logger.info("Unzipping war: "+originalWar.getName()+"...");
 			// Unzip the war to the temp dir
 			WarUtilities.unzipWarToDirectory(originalWar, tempDir);
 			// create the ebextensions directory
 			callback.accept(tempDir);
 			// Zip the war into a new file
 			File warCopy = File.createTempFile("WarCopy", ".war");
+			logger.info("Creating new war with .ebextensions: "+originalWar.getName()+"...");
 			WarUtilities.zipDirectoryToWar(tempDir, warCopy);
 			return warCopy;
 		} catch (IOException e) {
