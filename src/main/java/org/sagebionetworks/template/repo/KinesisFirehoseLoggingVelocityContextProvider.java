10596adbab111646f68f02b4cd9185b207bf9363
Ziming Dong <ziming.dong@sagebase.org>
2019-06-05 14:47:04 -0700

    kinesis velocity setup

---
+++ src/main/java/org/sagebionetworks/template/repo/KinesisFirehoseLoggingVelocityContextProvider.java
@@ -0,0 +1,22 @@
+package org.sagebionetworks.template.repo;
+
+import static org.sagebionetworks.template.Constants.KINESIS_FIREHOSE_STREAM_NAMES;
+import static org.sagebionetworks.template.Constants.PROPERTY_KEY_KINESIS_FIREHOSE_LOGGER_STREAM_NAMES;
+
+import org.apache.velocity.VelocityContext;
+import org.sagebionetworks.template.config.Configuration;
+
+public class KinesisFirehoseLoggingVelocityContextProvider implements VelocityContextProvider{
+
+	Configuration config;
+
+	public KinesisFirehoseLoggingVelocityContextProvider(Configuration config) {
+		this.config = config;
+	}
+
+	@Override
+	public void addToContext(VelocityContext context) {
+		String[] KinesisStreamNames = config.getComaSeparatedProperty(PROPERTY_KEY_KINESIS_FIREHOSE_LOGGER_STREAM_NAMES);
+		context.put(KINESIS_FIREHOSE_STREAM_NAMES, KinesisStreamNames);
+	}
+}
