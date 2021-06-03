94ab394677ae27ea799a3112365eee8c6127aa8b
Marco Marasca <8505576+marcomarasca@users.noreply.github.com>
2021-06-03 10:08:50 -0700

    PLFM-6763: Basic support for storage class transition rules

--- src/test/java/org/sagebionetworks/template/s3/S3ConfigValidatorTest.java
+++ src/test/java/org/sagebionetworks/template/s3/S3ConfigValidatorTest.java
@@ -5,12 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.amazonaws.services.s3.model.StorageClass;
 
 @ExtendWith(MockitoExtension.class)
 public class S3ConfigValidatorTest {
@@ -78,5 +81,122 @@ public class S3ConfigValidatorTest {
 		}).getMessage();
 
 		assertEquals("An inventory bucket is defined but was not in the list of buckets.", errorMessage);
+	}
+	
+	@Test
+	public void testValidateWithEmptyStorageClassTransitions() {
+		
+		S3BucketDescriptor bucket = new S3BucketDescriptor();
+		bucket.setName("bucket");
+		bucket.setStorageClassTransitions(Collections.emptyList());
+
+		when(mockConfig.getBuckets()).thenReturn(Arrays.asList(bucket));
+
+		// Call under test
+		validator.validate();
+	}
+	
+	@Test
+	public void testValidateWithStorageClassTransition() {
+		
+		S3BucketDescriptor bucket = new S3BucketDescriptor();
+		bucket.setName("bucket");
+		bucket.setStorageClassTransitions(Collections.singletonList(
+				new S3BucketClassTransition()
+					.withStorageClass(StorageClass.IntelligentTiering)
+					.withDays(30)
+		));
+
+		when(mockConfig.getBuckets()).thenReturn(Arrays.asList(bucket));
+
+		// Call under test
+		validator.validate();
+	}
+	
+	@Test
+	public void testValidateWithDuplicateStorageClassTransition() {
+		
+		S3BucketDescriptor bucket = new S3BucketDescriptor();
+		bucket.setName("bucket");
+		bucket.setStorageClassTransitions(Arrays.asList(
+				new S3BucketClassTransition()
+					.withStorageClass(StorageClass.IntelligentTiering)
+					.withDays(30),
+				new S3BucketClassTransition()
+					.withStorageClass(StorageClass.IntelligentTiering)
+					.withDays(10)
+		));
+
+		when(mockConfig.getBuckets()).thenReturn(Arrays.asList(bucket));
+
+		IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> {			
+			// Call under test
+			validator.validate();
+		});
+		
+		assertEquals("Duplicate storageClass transition found for bucket bucket: S3BucketClassTransition [storageClass=INTELLIGENT_TIERING, days=10]", ex.getMessage());
+	}
+	
+	@Test
+	public void testValidateWithStorageClassTransitionAndNoStorageClass() {
+		
+		S3BucketDescriptor bucket = new S3BucketDescriptor();
+		bucket.setName("bucket");
+		bucket.setStorageClassTransitions(Arrays.asList(
+				new S3BucketClassTransition()
+					.withStorageClass(null)
+					.withDays(30)
+		));
+
+		when(mockConfig.getBuckets()).thenReturn(Arrays.asList(bucket));
+
+		IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> {			
+			// Call under test
+			validator.validate();
+		});
+		
+		assertEquals("The storageClass for the transition in bucket bucket is required.", ex.getMessage());
+	}
+	
+	@Test
+	public void testValidateWithStorageClassTransitionAndNoDays() {
+		
+		S3BucketDescriptor bucket = new S3BucketDescriptor();
+		bucket.setName("bucket");
+		bucket.setStorageClassTransitions(Arrays.asList(
+				new S3BucketClassTransition()
+					.withStorageClass(StorageClass.IntelligentTiering)
+					.withDays(null)
+		));
+
+		when(mockConfig.getBuckets()).thenReturn(Arrays.asList(bucket));
+
+		IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> {			
+			// Call under test
+			validator.validate();
+		});
+		
+		assertEquals("The days value must be greater than 0 for transition in bucket bucket", ex.getMessage());
+	}
+	
+	@Test
+	public void testValidateWithStorageClassTransitionAndDaysLessThanOne() {
+		
+		S3BucketDescriptor bucket = new S3BucketDescriptor();
+		bucket.setName("bucket");
+		bucket.setStorageClassTransitions(Arrays.asList(
+				new S3BucketClassTransition()
+					.withStorageClass(StorageClass.IntelligentTiering)
+					.withDays(0)
+		));
+
+		when(mockConfig.getBuckets()).thenReturn(Arrays.asList(bucket));
+
+		IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> {			
+			// Call under test
+			validator.validate();
+		});
+		
+		assertEquals("The days value must be greater than 0 for transition in bucket bucket", ex.getMessage());
 	}
 }
