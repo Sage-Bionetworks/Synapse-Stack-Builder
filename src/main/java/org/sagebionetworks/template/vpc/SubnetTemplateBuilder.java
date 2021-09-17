fb38dabdb056d9f32194044f83cadf30f102cfbc
john-hill <john.hill@sagebase.org>
2021-09-16 19:03:12 -0700

    switch to one endpoint per color

--- src/main/java/org/sagebionetworks/template/vpc/SubnetTemplateBuilder.java
+++ src/main/java/org/sagebionetworks/template/vpc/SubnetTemplateBuilder.java
@@ -3,10 +3,4 @@ package org.sagebionetworks.template.vpc;
 public interface SubnetTemplateBuilder {
     public void buildAndDeployPublicSubnets() throws InterruptedException;
     public void buildAndDeployPrivateSubnets() throws InterruptedException;
-	/**
-	 * After each private subnet is built, this will build a VPC Endpoint that depends on all
-	 * subnet rout table IDs.
-	 * @throws InterruptedException
-	 */
-	void buildVPCEndpoint() throws InterruptedException;
 }
