bbe017dd7e206ca590cf112be03d2cb2a1bae2f5
Xavier Schildwachter <x.schildwachter@sagebase.org>
2022-09-20 23:19:30 -0700

    Junit5

--- src/test/java/org/sagebionetworks/template/repo/beanstalk/ElasticBeanstalkSolutionStackNameProviderImplTest.java
+++ src/test/java/org/sagebionetworks/template/repo/beanstalk/ElasticBeanstalkSolutionStackNameProviderImplTest.java
@@ -1,12 +1,17 @@
 package org.sagebionetworks.template.repo.beanstalk;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-import static org.mockito.Mockito.verify;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.any;
-import static org.sagebionetworks.template.repo.beanstalk.ElasticBeanstalkSolutionStackNameProviderImpl.AMI_VIRTUALIZATION_TYPE;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.sagebionetworks.template.repo.beanstalk.ElasticBeanstalkSolutionStackNameProviderImpl.AMI_VIRTUALIZATION_TYPE;
 import com.amazonaws.services.ec2.AmazonEC2Client;
 import com.amazonaws.services.elasticbeanstalk.AWSElasticBeanstalkClient;
 import com.amazonaws.services.elasticbeanstalk.model.CustomAmi;
@@ -16,14 +21,9 @@ import com.amazonaws.services.elasticbeanstalk.model.ListPlatformVersionsRequest
 import com.amazonaws.services.elasticbeanstalk.model.ListPlatformVersionsResult;
 import com.amazonaws.services.elasticbeanstalk.model.PlatformDescription;
 import com.amazonaws.services.elasticbeanstalk.model.PlatformSummary;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
 import org.sagebionetworks.template.config.RepoConfiguration;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class ElasticBeanstalkSolutionStackNameProviderImplTest {
 	@Mock
 	AWSElasticBeanstalkClient mockElasticBeanstalkClient;
@@ -42,7 +42,7 @@ public class ElasticBeanstalkSolutionStackNameProviderImplTest {
 
 	PlatformDescription platformDescription;
 
-	@Before
+	@BeforeEach
 	public void setUp(){
 		encrypter = new ElasticBeanstalkSolutionStackNameProviderImpl(mockElasticBeanstalkClient, mockEc2Client, mockConfig);
 		platformDescription = new PlatformDescription()
@@ -54,14 +54,12 @@ public class ElasticBeanstalkSolutionStackNameProviderImplTest {
 								.withVirtualizationType(AMI_VIRTUALIZATION_TYPE).withImageId(originalImageId),
 						new CustomAmi()
 								.withVirtualizationType("Wrong again").withImageId("wrong2"));
-
-		when(mockElasticBeanstalkClient.listPlatformVersions(any(ListPlatformVersionsRequest.class)))
-				.thenReturn(new ListPlatformVersionsResult().withPlatformSummaryList(new PlatformSummary().withPlatformArn(platformArn)));
-
 	}
 
 	@Test
-	public void testGetEncryptedElasticBeanstalkAMI(){
+	public void testGetSolutionStackName(){
+		when(mockElasticBeanstalkClient.listPlatformVersions(any(ListPlatformVersionsRequest.class)))
+				.thenReturn(new ListPlatformVersionsResult().withPlatformSummaryList(new PlatformSummary().withPlatformArn(platformArn)));
 		when(mockElasticBeanstalkClient.describePlatformVersion(new DescribePlatformVersionRequest()
 				.withPlatformArn(platformArn))).thenReturn(new DescribePlatformVersionResult().withPlatformDescription(platformDescription));
 
@@ -70,32 +68,42 @@ public class ElasticBeanstalkSolutionStackNameProviderImplTest {
 		assertEquals(expectedSolutionStackName, encrypter.getSolutionStackName("tomcatVersion", "javaVersion", "linuxVersion"));
 	}
 
-	@Test (expected = IllegalArgumentException.class)
-	public void testGetEncryptedElasticBeanstalkAMI_nullJavaVersion(){
-		encrypter.getPlatformArn(null, "9000.1", "1.2.3");
+	@Test
+	public void testGetSolutionStackName_nullJavaVersion(){
+		IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+			encrypter.getPlatformArn(null, "9000.1", "1.2.3");
+		});
 	}
 
-	@Test (expected = IllegalArgumentException.class)
-	public void testGetEncryptedElasticBeanstalkAMI_nullAmazonLinuxVersion(){
-		encrypter.getPlatformArn("-42", "9000.1", null);
+	@Test
+	public void testGetSolutionStackName_nullAmazonLinuxVersion(){
+		IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+			encrypter.getPlatformArn("-42", "9000.1", null);
+		});
 	}
 
-	@Test (expected = IllegalArgumentException.class)
-	public void testGetEncryptedElasticBeanstalkAMI_nullTomcatVersion(){
-		encrypter.getPlatformArn("-42", null, "1.2.3");
+	@Test
+	public void testGetSolutionStackName_nullTomcatVersion(){
+		IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+			encrypter.getPlatformArn("-42", null, "1.2.3");
+		});
 	}
 
-	@Test (expected = IllegalArgumentException.class)
+	@Test
 	public void testGetPlatformArn__noResults(){
 		when(mockElasticBeanstalkClient.listPlatformVersions(any(ListPlatformVersionsRequest.class)))
 				.thenReturn(new ListPlatformVersionsResult());
 
 		//method under test
-		encrypter.getPlatformArn("1", "2", "3");
+		IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+			encrypter.getPlatformArn("1", "2", "3");
+		});
 	}
 
 	@Test
 	public void testGetPlatformArn__resultFound(){
+		when(mockElasticBeanstalkClient.listPlatformVersions(any(ListPlatformVersionsRequest.class)))
+			.thenReturn(new ListPlatformVersionsResult().withPlatformSummaryList(new PlatformSummary().withPlatformArn(platformArn)));
 		//method under test
 		String arnResult = encrypter.getPlatformArn("1", "2", "3");
 
