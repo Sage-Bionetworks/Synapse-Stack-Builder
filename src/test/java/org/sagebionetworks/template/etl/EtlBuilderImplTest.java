3aa81efeb755083df92b5b8854f3c0aea5e9985b
SandhraSokhal <sandhra.sokhal@sagebase.org>
2023-03-03 12:09:44 -0800

    test to check database name

--- src/test/java/org/sagebionetworks/template/etl/EtlBuilderImplTest.java
+++ src/test/java/org/sagebionetworks/template/etl/EtlBuilderImplTest.java
@@ -3,6 +3,7 @@ package org.sagebionetworks.template.etl;
 import com.amazonaws.services.cloudformation.model.Tag;
 import org.apache.logging.log4j.Logger;
 import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -31,16 +32,22 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.sagebionetworks.template.Constants.ETL_DESCRIPTORS;
+import static org.sagebionetworks.template.Constants.GLUE_DATABASE_NAME;
 import static org.sagebionetworks.template.Constants.PROPERTY_KEY_INSTANCE;
 import static org.sagebionetworks.template.Constants.PROPERTY_KEY_STACK;
+import static org.sagebionetworks.template.Constants.STACK;
 import static org.sagebionetworks.template.Constants.TEMPLATE_ETL_GLUE_JOB_RESOURCES;
+import static org.sagebionetworks.template.etl.EtlBuilderImpl.GLUE_DB_NAME;
 
 @ExtendWith(MockitoExtension.class)
 public class EtlBuilderImplTest {
-    private static String STACK = "dev";
+    private static String STACK_NAME = "dev";
     private static String INSTANCE = "test";
     @Captor
     ArgumentCaptor<CreateOrUpdateStackRequest> requestCaptor;
+    @Captor
+    ArgumentCaptor<VelocityContext> velocityContextCaptor;
     @Mock
     private CloudFormationClient cloudFormationClient;
     @Mock
@@ -59,24 +66,25 @@ public class EtlBuilderImplTest {
     private LoggerFactory loggerFactory;
     private EtlBuilderImpl etlBuilderImpl;
     private List<Tag> tags = new ArrayList<>();
+    private List<EtlDescriptor> etlDescriptors = new ArrayList<>();
 
     @BeforeEach
     public void before() {
         tags = new LinkedList<>();
         Tag t = new Tag().withKey("aKey").withValue("aValue");
         tags.add(t);
-        List<EtlDescriptor> etlDescriptors = new ArrayList<>();
         EtlDescriptor etlDescriptor = new EtlDescriptor();
         etlDescriptor.setName("processAccessRecord");
         etlDescriptor.setScriptLocation("S3://${stack}.fakeBucket/");
         etlDescriptor.setDestinationPath("S3://${stack}.destination/");
         etlDescriptor.setSourcePath("S3://${stack}.source/");
         etlDescriptors.add(etlDescriptor);
-        when(mockConfig.getProperty(PROPERTY_KEY_STACK)).thenReturn(STACK);
+        when(mockConfig.getProperty(PROPERTY_KEY_STACK)).thenReturn(STACK_NAME);
         when(mockConfig.getProperty(PROPERTY_KEY_INSTANCE)).thenReturn(INSTANCE);
         when(etlConfig.getEtlDescriptors()).thenReturn(etlDescriptors);
         when(tagsProvider.getStackTags()).thenReturn(tags);
         when(loggerFactory.getLogger(EtlBuilderImpl.class)).thenReturn(logger);
+
         when(velocityEngine.getTemplate(any(String.class))).thenReturn(mockTemplate);
         doAnswer(new Answer<Void>() {
             @Override
@@ -92,8 +100,13 @@ public class EtlBuilderImplTest {
     @Test
     public void testEtlBuildAndDeployJob() {
         String expectedStackName = new StringJoiner("-")
-                .add(STACK).add(INSTANCE).add("etl").toString();
+                .add(STACK_NAME).add(INSTANCE).add("etl").toString();
         etlBuilderImpl.buildAndDeploy();
+        verify(mockTemplate).merge(velocityContextCaptor.capture(), any());
+        VelocityContext context = velocityContextCaptor.getValue();
+        assertEquals(GLUE_DB_NAME, context.get(GLUE_DATABASE_NAME));
+        assertEquals(STACK_NAME, context.get(STACK));
+        assertEquals(etlDescriptors, context.get(ETL_DESCRIPTORS));
         verify(velocityEngine).getTemplate(TEMPLATE_ETL_GLUE_JOB_RESOURCES);
         verify(cloudFormationClient).createOrUpdateStack(requestCaptor.capture());
         CreateOrUpdateStackRequest req = requestCaptor.getValue();
