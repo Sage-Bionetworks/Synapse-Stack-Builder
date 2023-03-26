bd258d1671370c75a3bbaa3ceb722ee32ab13c3d
SandhraSokhal <sandhra.sokhal@sagebase.org>
2023-03-26 09:44:34 -0700

    Renaming the getter setter method names

--- src/main/java/org/sagebionetworks/template/datawarehouse/EtlJobConfig.java
+++ src/main/java/org/sagebionetworks/template/datawarehouse/EtlJobConfig.java
@@ -8,24 +8,24 @@ import java.util.Objects;
 public class EtlJobConfig {
     private List<EtlJobDescriptor> etlJobDescriptors = new ArrayList<>();
 
-    public List<EtlJobDescriptor> getEtlDescriptors() {
+    public List<EtlJobDescriptor> getEtlJobDescriptors() {
         return etlJobDescriptors;
-    }
-
-    public void setEtlDescriptors(List<EtlJobDescriptor> etlJobDescriptors) {
-        this.etlJobDescriptors = etlJobDescriptors;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        EtlJobConfig etlJobConfig = (EtlJobConfig) o;
-        return Objects.equals(etlJobDescriptors, etlJobConfig.etlJobDescriptors);
+        EtlJobConfig that = (EtlJobConfig) o;
+        return Objects.equals(etlJobDescriptors, that.etlJobDescriptors);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(etlJobDescriptors);
+    }
+
+    public void setEtlJobDescriptors(List<EtlJobDescriptor> etlJobDescriptors) {
+        this.etlJobDescriptors = etlJobDescriptors;
     }
 }
