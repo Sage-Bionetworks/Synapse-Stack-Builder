df138559f6a8ca35ac8e5e6b0b45de305500fdc4
Marco Marasca <8505576+marcomarasca@users.noreply.github.com>
2025-04-10 14:51:08 -0700

    PLFM-8923: Add WAF rule (Count) for geo restriction (https://grants.nih.gov/grants/guide/notice-files/NOT-OD-25-083.html) (#706)

--- src/test/java/org/sagebionetworks/template/repo/RepositoryTemplateBuilderImplTest.java
+++ src/test/java/org/sagebionetworks/template/repo/RepositoryTemplateBuilderImplTest.java
@@ -845,13 +845,13 @@ public class RepositoryTemplateBuilderImplTest {
 		JSONObject webAcl = resources.getJSONObject("prod101WebACL");
 		JSONObject props = webAcl.getJSONObject("Properties");
 		JSONArray rules = props.getJSONArray("Rules");
-		assertEquals(11, rules.length());
+		assertEquals(12, rules.length());
 			
-		JSONObject adminRule = rules.getJSONObject(10);
+		JSONObject adminRule = rules.getJSONObject(11);
 		assertEquals("prod-101-Admin-Access-Rule",adminRule.get("Name"));
 		assertEquals("{\"Block\":{}}",adminRule.getJSONObject("Action").toString());
 
-		JSONObject sizeRestrictionsRule = rules.getJSONObject(1);
+		JSONObject sizeRestrictionsRule = rules.getJSONObject(2);
 		assertEquals("prod-101-size-restrictions-rule", sizeRestrictionsRule.get("Name"));
 		JSONObject statement = sizeRestrictionsRule.getJSONObject("Statement");
 		JSONArray ruleSizeStatements = statement.getJSONObject("OrStatement").getJSONArray("Statements");
