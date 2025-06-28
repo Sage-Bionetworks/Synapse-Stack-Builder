387512838c2ec6f80df61d96430e6b3adbfa8c02
Xavier Schildwachter <x.schildwachter@sagebase.org>
2025-06-28 13:30:08 -0700

    Add VPC to lambda

--- src/test/java/org/sagebionetworks/template/markdownit/MarkDownItLambdaBuilderImplTest.java
+++ src/test/java/org/sagebionetworks/template/markdownit/MarkDownItLambdaBuilderImplTest.java
@@ -23,6 +23,7 @@ import java.util.Collections;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
