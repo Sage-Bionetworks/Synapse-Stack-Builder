d3c72ac37e81a0845768dec9bc37033b83e0c381
Bruce Hoff <bruce.hoff@sagebase.org>
2026-04-22 19:05:46 -0700

    Plfm 9548 (#849)
    PLFM-9548

--- src/main/java/org/sagebionetworks/template/repo/ecs/DockerImageBuilderImpl.java
+++ src/main/java/org/sagebionetworks/template/repo/ecs/DockerImageBuilderImpl.java
@@ -28,6 +28,7 @@ public class DockerImageBuilderImpl implements DockerImageBuilder {
 	static final String TEMPLATE_DOCKERFILE = "templates/repo/ecs/Dockerfile.vpt";
 	static final String TEMPLATE_SERVER_XML = "templates/repo/ecs/server.xml.vpt";
 	static final String TEMPLATE_STARTUP_SH = "templates/repo/ecs/startup.sh.vpt";
+	static final String TEMPLATE_NGINX_CONF = "templates/repo/ecs/nginx.conf.vpt";
 
 	private final ArtifactDownload downloader;
 	private final CertificateBuilder certificateBuilder;
@@ -84,27 +85,10 @@ public class DockerImageBuilderImpl implements DockerImageBuilder {
 		try {
 			File buildDir = Files.createTempDirectory("docker-build-").toFile();
 
-			// Generate self-signed TLS certificate
+			// Generate self-signed TLS certificate used by nginx on the public listener
 			CertificatePair certPair = certificateBuilder.buildNewX509CertificatePair();
-
-			// Write certificate and key files
-			File serverCrt = new File(buildDir, "server.crt");
-			File serverKey = new File(buildDir, "server.key");
-			writeFile(serverCrt, certPair.getX509CertificatePEM());
-			writeFile(serverKey, certPair.getPrivateKeyPEM());
-			
-			// create the server.keystore file from the server.crt and server.key files
-			File serverKeystore = new File(buildDir, "server.keystore");
-			executeCommand(
-				"yum install -y openssl",
-				"&& yum clean all",
-				"&& openssl pkcs12 -export",
-				"-in", serverCrt.getAbsolutePath(),
-				"-inkey", serverKey.getAbsolutePath(),
-				"-out", serverKeystore.getAbsolutePath(),
-				"-name tomcat",
-				"-passout pass:changeit"
-			);
+			writeFile(new File(buildDir, "server.crt"), certPair.getX509CertificatePEM());
+			writeFile(new File(buildDir, "server.key"), certPair.getPrivateKeyPEM());
 
 			// Copy WAR file
 			String s3Key = environment.createS3Key(version, number);
@@ -119,8 +103,11 @@ public class DockerImageBuilderImpl implements DockerImageBuilder {
 			context.put("warFileName", warFileName);
 			renderTemplate(TEMPLATE_SERVER_XML, context, new File(buildDir, "server.xml"));
 
-			// Render startup script (converts env vars to JVM -D flags)
+			// Render startup script (launches nginx, converts env vars to JVM -D flags)
 			renderTemplate(TEMPLATE_STARTUP_SH, context, new File(buildDir, "startup.sh"));
+
+			// Render nginx.conf (TLS terminator + reverse proxy to Tomcat)
+			renderTemplate(TEMPLATE_NGINX_CONF, context, new File(buildDir, "nginx.conf"));
 
 			// Render Dockerfile template
 			int taskMemory = config.getIntegerProperty(PROPERTY_KEY_ECS_TASK_MEMORY);
