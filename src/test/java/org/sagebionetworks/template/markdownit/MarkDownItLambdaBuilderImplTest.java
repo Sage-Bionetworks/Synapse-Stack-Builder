ad3e6e227fa54f970922215f244842590f355282
xschildw <x.schildwachter@sagebase.org>
2026-07-12 20:01:07 -0700

    Fix test

--- src/test/java/org/sagebionetworks/template/markdownit/MarkDownItLambdaBuilderImplTest.java
+++ src/test/java/org/sagebionetworks/template/markdownit/MarkDownItLambdaBuilderImplTest.java
@@ -53,8 +53,6 @@ public class MarkDownItLambdaBuilderImplTest {
 
     @BeforeEach
     public void before() throws Exception {
-        stack = "dev";
-        when(mockConfig.getProperty(PROPERTY_KEY_STACK)).thenReturn(stack);
         when(mockConfig.getProperty(PROPERTY_KEY_LAMBDA_MARKDOWNIT_VERSION)).thenReturn("v0.0.1");
         when(mockConfig.getProperty(PROPERTY_KEY_LAMBDA_MARKDOWN_IT_SUBDOMAIN)).thenReturn("md2html");
         when(mockConfig.getProperty(PROPERTY_KEY_LAMBDA_MARKDOWNIT_CERTIFICATE_ARN)).thenReturn("arn:123456789012:cert");
@@ -62,6 +60,8 @@ public class MarkDownItLambdaBuilderImplTest {
 
     @Test
     public void testBuildMarkDownItLambda() throws Exception {
+        stack = "dev";
+        when(mockConfig.getProperty(PROPERTY_KEY_STACK)).thenReturn(stack);
 
         testFile = File.createTempFile("markdown-it-v0.0.1", "zip");
         velocityEngine = new TemplateGuiceModule().velocityEngineProvider();
@@ -89,7 +89,7 @@ public class MarkDownItLambdaBuilderImplTest {
         // call under test
         builder.buildMarkDownItLambda();
 
-        verify(mockDownloader).downloadFile("https://sagebionetworks.jfrog.io/lambda/org/sagebase/markdownit/markdown-it-v0.0.1.zip");
+        verify(mockDownloader).downloadFile("https://sagebionetworks.jfrog.io/artifactory/lambda-artifacts/org/sagebionetworks/markdown-it/markdown-it-v0.0.1.zip");
 
         ArgumentCaptor<PutObjectRequest> putRequestCaptor = ArgumentCaptor.forClass(PutObjectRequest.class);
         ArgumentCaptor<RequestBody> requestBodyCaptor = ArgumentCaptor.forClass(RequestBody.class);
@@ -134,4 +134,79 @@ public class MarkDownItLambdaBuilderImplTest {
 
     }
 
+    @Test
+    public void testBuildProdMarkDownItLambda() throws Exception {
+        stack = "prod";
+        when(mockConfig.getProperty(PROPERTY_KEY_STACK)).thenReturn(stack);
+
+        testFile = File.createTempFile("markdown-it-v0.0.1", "zip");
+        velocityEngine = new TemplateGuiceModule().velocityEngineProvider();
+
+        MarkDownItLambdaBuilder builder = new MarkDownItLambdaBuilderImpl(
+                mockConfig,
+                mockDownloader,
+                mockCloudFormationClientWrapper,
+                mockTagsProvider,
+                mockS3Client,
+                velocityEngine);
+
+
+        when(mockDownloader.downloadFile(any())).thenReturn(testFile);
+
+        when(mockTagsProvider.getStackTags(mockConfig)).thenReturn(Collections.emptyList());
+
+        Stack markdownItLambdaStack = Stack.builder().build();
+
+        when(mockCloudFormationClientWrapper.describeStack(any())).thenReturn(Optional.of(markdownItLambdaStack));
+
+        String expectedBucket = "prod.artifacts.sagebase.org";
+        String expectedKey = "markdown-it-v0.0.1.zip";
+
+        // call under test
+        builder.buildMarkDownItLambda();
+
+        verify(mockDownloader).downloadFile("https://sagebionetworks.jfrog.io/artifactory/lambda-artifacts/org/sagebionetworks/markdown-it/markdown-it-v0.0.1.zip");
+
+        ArgumentCaptor<PutObjectRequest> putRequestCaptor = ArgumentCaptor.forClass(PutObjectRequest.class);
+        ArgumentCaptor<RequestBody> requestBodyCaptor = ArgumentCaptor.forClass(RequestBody.class);
+        verify(mockS3Client).putObject(putRequestCaptor.capture(), requestBodyCaptor.capture());
+        assertNotNull(requestBodyCaptor.getValue());
+        PutObjectRequest capturedRequest = putRequestCaptor.getValue();
+        assertEquals(expectedBucket, capturedRequest.bucket());
+        assertEquals(expectedKey, capturedRequest.key());
+
+        ArgumentCaptor<CreateOrUpdateStackRequest> argCaptorCreateOrUpdateStack = ArgumentCaptor.forClass(CreateOrUpdateStackRequest.class);
+        ArgumentCaptor<String> argCaptorWaitForStack = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> argCaptorDescribeStack = ArgumentCaptor.forClass(String.class);
+
+        verify(mockCloudFormationClientWrapper, times(1)).createOrUpdateStack(argCaptorCreateOrUpdateStack.capture());
+        verify(mockCloudFormationClientWrapper, times(1)).waitForStackToComplete(argCaptorWaitForStack.capture());
+        verify(mockCloudFormationClientWrapper, times(1)).describeStack(argCaptorDescribeStack.capture());
+
+        CreateOrUpdateStackRequest request = argCaptorCreateOrUpdateStack.getValue();
+        assertEquals("prod-markdown-it-function", request.getStackName());
+        assertTrue(request.getTags().isEmpty());
+        assertEquals(1, request.getCapabilities().length);
+        assertEquals(Capability.CAPABILITY_NAMED_IAM, request.getCapabilities()[0]);
+        assertNotNull(request.getTemplateBody());
+
+        JSONObject templateJson = new JSONObject(request.getTemplateBody());
+        System.out.println(request.getTemplateBody());
+        JSONObject resources = templateJson.getJSONObject("Resources");
+        assertTrue(resources.has("mdlambdaServiceRole"));
+        assertTrue(resources.has("mdlambda"));
+        assertTrue(resources.has("MarkdownItApi"));
+        assertTrue(resources.has("MarkdownItApiPostMethod"));
+        assertTrue(resources.has("MarkdownItApiPermission"));
+        assertTrue(resources.has("MarkdownItApiDeployment"));
+        assertTrue(resources.has("MarkdownItApiStage"));
+        assertTrue(resources.has("CustomDomain"));
+        assertTrue(resources.has("BasePathMapping"));
+
+        assertEquals("prod-markdown-it-function", argCaptorWaitForStack.getValue());
+        assertEquals("prod-markdown-it-function", argCaptorDescribeStack.getValue());
+
+        assertFalse(testFile.exists());
+
+    }
 }
